07d13548400ad1f3cb3ab26d7d375d3b75101855
Roman Penn <pnnroman92@gmail.com>
2022-01-18 16:44:05 -0600

    added logic for Room subclasses and test classes

--- src/com/Hotel/Room/PresidentialSuite.java
+++ src/com/Hotel/Room/PresidentialSuite.java
@@ -1,4 +1,30 @@
 package com.Hotel.Room;
 
+import java.time.LocalDate;
+
 public class PresidentialSuite extends Room{
+    private final double COST_PER_NIGHT = 235.89;
+    private final int MAX_PEOPLE = 5;
+    private final int FIRST_ROOM_NUMBER = 200;
+    private final int LAST_ROOM_NUMBER = 210;
+    private String guestName;
+    private int guestNumber;
+    private int daysReserved;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private int roomNumber;
+
+
+    public PresidentialSuite(String guestName,int guestNumber,int daysReserved, String checkInDate) {
+        setGuestName(guestName);
+        setGuestNumber(guestNumber);
+        setDaysReserved(daysReserved);
+        setCheckInDate(checkInDate);
+        setRoomNumber(FIRST_ROOM_NUMBER,LAST_ROOM_NUMBER);
+    }
+
+
+    public double calculateCost() {
+        return COST_PER_NIGHT * getDaysReserved();
+    }
 }
